d56bcc806e67459a13f41f969e4b5ed28f2048bb
gadji <gadji058@bk.ru>
2022-12-31 14:47:19 +0300

    init

---
+++ konkretniy/blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('<slug:slug>', views.PostListView.as_view(), name='post_list')
+]
