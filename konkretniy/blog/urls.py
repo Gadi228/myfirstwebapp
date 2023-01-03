54a84124a8cc536f54a75592cbd615277eb28cce
gadji <gadji058@bk.ru>
2023-01-03 15:30:39 +0300

    Добавлена страница Home и ckeditor

--- konkretniy/blog/urls.py
+++ konkretniy/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('<slug:slug>/', views.PostListView.as_view(), name='post_detail'),
+    path('', views.HomeView.as_view(), name="home"),
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'),
     path('recipe', views.recipe, name='recipe'),
 ]
--- konkretniy/config/settings.py
+++ konkretniy/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
 
     'mptt',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,7 @@ STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
